deb900126269dca7bc4d2a6fd1609bd9c4feb263
ipavka <ipavka@ya.ru>
2022-01-20 18:11:45 +0300

    new kata

--- main.js
+++ main.js
@@ -17,4 +17,37 @@ console.log(list([{name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'}]))
 console.log(list([ {name: 'Bart'}, {name: 'Lisa'} ]))
 console.log(list([ {name: 'Bart'} ]))
 console.log(list([]))
+console.log('*'.repeat(30))
 
+/* https://www.codewars.com/kata/545cedaa9943f7fe7b000048/train/javascript */
+
+const inStr = "The quick brown fox jumps over the lazy dog."
+const inStr1 = "This is not a pangram."
+function isPangram(string){
+    const size = new Set(string.replace(/[^a-z]/gi, '').toLowerCase()).size
+    return size === 26
+}
+
+console.log(isPangram(inStr))
+console.log(isPangram(inStr1))
+console.log('*'.repeat(30))
+
+/* https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/train/javascript */
+
+function duplicateEncode(word){
+
+}
+
+console.log(duplicateEncode("din"))
+console.log(duplicateEncode("recede"))
+console.log(duplicateEncode("Success"))
+console.log(duplicateEncode("(( @"))
+
+/*
+
+"din"      =>  "((("
+"recede"   =>  "()()()"
+"Success"  =>  ")())())"
+"(( @"     =>  "))(("
+
+*/
