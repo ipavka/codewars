2697fd405dbb1a57053944151a83e9d40b759550
ipavka <ipavka@ya.ru>
2022-01-23 20:15:04 +0300

    new kata WR

--- 6_kyu.js
+++ 6_kyu.js
@@ -121,9 +121,15 @@ console.log(isSolved( [// Ничья
 console.log('*'.repeat(30))
 
 
+/* https://www.codewars.com/kata/52774a314c2333f0a7000688/train/javascript */
+
+function validParentheses(parens) {
+
+}
 
 
 
+console.log('*'.repeat(30))
 
 
 
