0a292f2c54c33775b061dee7acc7182825aa50c1
ipavka <ipavka@ya.ru>
2022-01-27 22:52:47 +0300

    hm

--- findamentals.js
+++ findamentals.js
@@ -103,3 +103,34 @@ console.log(SeriesSum(5)) // "1.57"
 console.log('*'.repeat(30))
 
 
+/* https://www.codewars.com/kata/5626b561280a42ecc50000d1 */
+
+
+const sumDigPow = (start, fin) => {
+    let result = [];
+
+    function exponen(num) {
+        let count = 0;
+        for (let i = 1; i <= num.length; i++) {
+            count += num[i-1]**i
+        }
+        return count == num
+    }
+
+    for (let i = start; i <= fin; i++) {
+        if(exponen(String(i))) result.push(i);
+    }
+    return result
+}
+
+
+console.log(sumDigPow(1, 10)) // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(sumDigPow(1, 100)) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 89]
+console.log(sumDigPow(10, 100)) //  [89]
+console.log(sumDigPow(90, 100)) // []
+console.log(sumDigPow(90, 150)) // [135]
+console.log(sumDigPow(50, 150)) // [89, 135]
+console.log(sumDigPow(10, 150)) // [89, 135]
+
+console.log('*'.repeat(30))
+
