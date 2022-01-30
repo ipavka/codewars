b956daea5dfd9442951c22f54feabeebf0bc5a8d
ipavka <ipavka@ya.ru>
2022-01-30 09:36:58 +0300

    HM

--- findamentals.js
+++ findamentals.js
@@ -153,3 +153,35 @@ console.log('*'.repeat(30))
 
 
 /* https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/train/javascript */
+
+
+function sortArray(array) {
+    const oddArray = [];
+
+    array.forEach(element => {
+        if(element % 2 !== 0) {
+          oddArray.push(element)
+          oddArray.sort((a,b) => {
+              return b - a
+            });
+        }
+      });
+    return array.reduce((acc, item) => {
+        if (item % 2 !== 0) {
+          acc.push(oddArray.pop())
+        } else {
+          acc.push(item)
+        }
+        return acc
+      }, [])
+  }
+
+
+console.log(sortArray([5, 3, 2, 8, 1, 4])) //  [1, 3, 2, 8, 5, 4]
+console.log(sortArray([5, 3, 1, 8, 0])) //  [1, 3, 5, 8, 0]
+console.log(sortArray([])) // []
+console.log(sortArray([7, 1])) // [1, 7]
+console.log(sortArray([5, 8, 6, 3, 4]))  // [3, 8, 6, 5, 4]
+console.log(sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]))  // [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+console.log('*'.repeat(30))
