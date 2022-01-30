44da97bbe9aa782c573bfe0edb01bb837adebae2
ipavka <ipavka@ya.ru>
2022-01-30 12:00:31 +0300

    HM

--- findamentals.js
+++ findamentals.js
@@ -198,3 +198,17 @@ function filter_list(array) {
 console.log(filter_list([1,2,'a','b'])) // [1,2]
 console.log(filter_list([1,'a','b',0,15])) // [1,0,15]
 console.log(filter_list([1,2,'aasf','1','123',123])) // [1,2,123]
+
+console.log('*'.repeat(30))
+
+/* https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/javascript */
+
+function nbYear(p0, percent, aug, p) {
+   
+}
+
+console.log(nbYear(1500, 5, 100, 5000)) // 15
+console.log(nbYear(1500000, 2.5, 10000, 2000000)) // 10
+console.log(nbYear(1500000, 0.25, 1000, 2000000)) // 94
+
+console.log('*'.repeat(30))
