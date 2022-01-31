614550f8d6efb50fc0e2fa61d218b29565bac1fc
ipavka <ipavka@ya.ru>
2022-01-31 08:59:06 +0300

    HM

---
+++ 5_kyu.js
@@ -0,0 +1,11 @@
+/* https://www.codewars.com/kata/5254ca2719453dcc0b00027d/train/javascript */
+
+
+function permutations(string) {
+  
+}
+
+
+console.log(permutations('a')) // ['a']
+console.log(permutations('ab')) // ['ab', 'ba']
+console.log(permutations('aabb')) // ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
