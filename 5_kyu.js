ec994e8474ee5a70842e22b9c37d5e61abf9e458
ipavka <ipavka@ya.ru>
2022-02-03 12:07:50 +0300

    work

--- 5_kyu.js
+++ 5_kyu.js
@@ -34,3 +34,31 @@ console.log(alphanumeric("     ")) // false
 console.log(alphanumeric("")) // false
 
 console.log('*'.repeat(30))
+
+
+/* https://www.codewars.com/kata/54e6533c92449cc251001667/train/javascript */
+
+const uniqueInOrder = (iterable) => {
+    let inData = typeof iterable !== 'string' ? iterable : iterable.split('');
+    let resArr = []
+    for (let i = 0; i < inData.length; i++) {
+        const current = inData[i];
+        const next = inData[i+1];
+        if (current !== next) {
+            resArr.push(inData[i])
+        }
+    }
+    return resArr
+}
+
+// const uniqueInOrder = (iterable) => { // вариант из решений
+//     return [...iterable].filter((a, i) => a !== iterable[i-1])
+// }
+
+console.log(uniqueInOrder('AAAABBBCCDAABBB')) // ['A','B','C','D','A','B']
+console.log(uniqueInOrder('ABBCcAD')) // ['A', 'B', 'C', 'c', 'A', 'D']
+console.log(uniqueInOrder([1, 2, 2, 3, 3])) // [1,2,3]
+
+
+console.log('*'.repeat(30))
+
