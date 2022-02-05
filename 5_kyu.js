7087a5a2f7c2b7d000c740bd357e7926880b5816
ipavka <ipavka@ya.ru>
2022-02-05 13:29:04 +0300

    work

--- 5_kyu.js
+++ 5_kyu.js
@@ -86,6 +86,27 @@ console.log(wave("")) // []
 console.log(wave("two words")) // ["Two words", "tWo words", "twO words", "two Words", "two wOrds", "two woRds", "two worDs", "two wordS"]
 console.log(wave(" gap ")) // [" Gap ", " gAp ", " gaP "]
 
+console.log('*'.repeat(30))
+
+
+/* https://www.codewars.com/kata/5544c7a5cb454edb3c000047/train/javascript */
+
+function bouncingBall(h, bounce, window) {
+    if (h > 0 && bounce > 0 && bounce < 1 && window < h) {
+        let result = -1;
+        let ballBounces = h
+
+        while (ballBounces > window) {
+            ballBounces *= bounce
+            result += 2
+        }
+        return result
+    } else return -1
+}
+
+console.log(bouncingBall(3.0, 0.66, 1.5)) // 3
+console.log(bouncingBall(30.0, 1, 1.5)) // 15
 
 console.log('*'.repeat(30))
+
 
