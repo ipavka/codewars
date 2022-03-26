d66af6d91042f67bc6fac39c9bef27f67436c5cb
ipavka <ipavka@ya.ru>
2022-03-26 08:25:00 +0300

    HM

--- 6_kyu.js
+++ 6_kyu.js
@@ -304,3 +304,20 @@ console.log(amountOfPages(660))
 
 console.log('*'.repeat(30));
 
+
+/* https://www.codewars.com/kata/5264d2b162488dc400000001/train/javascript */
+
+
+const spinWords = (string) => {
+    //TODO Have fun :)
+  }
+
+  console.log(spinWords("Welcome")) // "emocleW"
+  console.log(spinWords("Hey fellow warriors")) // "Hey wollef sroirraw"
+  console.log(spinWords("This is a test")) // "This is a test"
+  console.log(spinWords("This is another test")) // "This is rehtona test"
+  console.log(spinWords("You are almost to the last test")) // "You are tsomla to the last test"
+  console.log(spinWords("Just kidding there is still one more")) // "Just gniddik ereht is llits one more"
+  console.log(spinWords("Seriously this is the last one")) // "ylsuoireS this is the last one"
+
+  console.log('*'.repeat(30));
