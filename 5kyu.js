b09f907ad5ec8f683bdd22baf0e06bf76e6d9a2a
iPavka <ipavka@ya.ru>
2023-04-09 12:18:16 +0200

    April_2_1

--- 5kyu.js
+++ 5kyu.js
@@ -17,3 +17,16 @@ console.log(rgb(173, 255, 47)); // 'ADFF2F'
 console.log(rgb(148, 0, 211)); // '9400D3'
 
 console.log('*'.repeat(30));
+
+/* https://www.codewars.com/kata/581fc49b55c3d2d83c0000f8/javascript */
+
+function arrayDiffVeryFast(a, b) {
+  const setArrayB = new Set(b);
+  return a.filter((el) => !setArrayB.has(el));
+}
+
+console.log(arrayDiffVeryFast([1, 2, 2, 2, 3], [2])); // [1, 3]
+console.log(arrayDiffVeryFast([1, 2, 2, 2, 3, 5, 5, 1, 2, 4, 5, 1], [2, 1, 5])); // [3,4]
+console.log(arrayDiffVeryFast([1, 2], [1])); // [2]
+
+console.log('*'.repeat(30));
