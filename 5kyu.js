6d447adf62004c8e7084d285bf041880a45a2812
iPavka <ipavka@ya.ru>
2023-04-16 11:38:58 +0200

    April_3

--- 5kyu.js
+++ 5kyu.js
@@ -30,3 +30,25 @@ console.log(arrayDiffVeryFast([1, 2, 2, 2, 3, 5, 5, 1, 2, 4, 5, 1], [2, 1, 5]));
 console.log(arrayDiffVeryFast([1, 2], [1])); // [2]
 
 console.log('*'.repeat(30));
+
+/* https://www.codewars.com/kata/52685f7382004e774f0001f7/javascript */
+
+function humanReadable(seconds) {
+  const parsHours = String(Math.floor(seconds / 3600)).padStart(2, '0');
+  const parsMinutes = String(Math.floor(seconds / 60) % 60).padStart(2, '0');
+  const parsSeconds = String(Math.floor(seconds % 60)).padStart(2, '0');
+  return `${parsHours}:${parsMinutes}:${parsSeconds}`;
+}
+
+console.log(humanReadable(0)); // '00:00:00'
+console.log(humanReadable(59)); // '00:00:59'
+console.log(humanReadable(60)); // '00:01:00'
+console.log(humanReadable(90)); // '00:01:30'
+console.log(humanReadable(3599)); // '00:59:59'
+console.log(humanReadable(3600)); // '01:00:00'
+console.log(humanReadable(45296)); // '12:34:56'
+console.log(humanReadable(86399)); // '23:59:59'
+console.log(humanReadable(86400)); // '24:00:00'
+console.log(humanReadable(359999)); // '99:59:59'
+
+console.log('*'.repeat(30));
